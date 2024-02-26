16d8371b16bba393f01a5db052c9795f24d61adb
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2024-02-26 14:24:25 -0600

    animations to buttons

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,9 +1,29 @@
-let box = document.getElementById('box1')
-/*
-box.onmouseover = function() {
-    box.style.backgroundColor = 'blue'
+let playerText = document.querySelector('#playerText')
+let computerText = document.querySelector('#computerText')
+let resultText = document.querySelector('#resultText')
+let choiceBtn = document.querySelectorALL(".button")
+
+let player;
+let computer;
+let result;
+
+choiceBtn.forEach(button => button.addeventlistener(click, () => {
+    player = button.textContent;
+    computerTurn()
+
+}))
+
+function computerTurn() {
+    const randNum = Math.floor(Math.random()* 3)+ 1
+    switch(randNum){
+        case 1:
+            computer = 'Rock'
+            break
+        case 2:
+            computer = 'Paper'
+            break
+        case 3:
+            computer = 'Scissors'
+            break
+    }
 }
-box.onmouseout = function() {
-    box.style.backgroundColor = rgb(20, 20, 23)
-}
-*/
