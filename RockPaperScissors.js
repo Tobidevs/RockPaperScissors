e717f6f06d6da8e0c901567bf21198cda715f83c
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2024-02-26 02:24:07 -0600

    fulll animations for options

---
+++ RockPaperScissors.js
@@ -0,0 +1,9 @@
+let box = document.getElementById('box1')
+/*
+box.onmouseover = function() {
+    box.style.backgroundColor = 'blue'
+}
+box.onmouseout = function() {
+    box.style.backgroundColor = rgb(20, 20, 23)
+}
+*/
